08df5224bba50a0a0859d3a293acafa19a8c3b5a
dilwong <dilwong@gmail.com>
2019-04-06 17:20:45 -0400

    Refactor Keithley 2450 SMU controller

--- Python/keithley2450.py
+++ Python/keithley2450.py
@@ -7,6 +7,7 @@ import visa
 import time
 import thread
 import socket
+import traceback
 
 class keithley2450:
 
@@ -41,23 +42,41 @@ class keithley2450:
             if listen_string == 'QUIT':
                 s.close()
             else:
-                listen_commands = listen_string.split()
-                listen_args = '(' + ','.join(listen_commands[1:]) + ')'
-                if listen_commands[0] in dir(self)[3:]:
-                    try:
-                        exec('result = self.' + listen_commands[0] + listen_args)
-                        if result:
-                            conn.sendall(str(result) + '\n')
-                        else:
-                            conn.sendall('NO DATA\n')
-                    except TypeError:
-                        conn.sendall('ERROR: TYPE ERROR\n')
-                else:
-                    conn.sendall('ERROR: COMMAND ERROR\n')
+                try:
+                    listen_commands = listen_string.split()
+                    listen_args = '(' + ','.join(listen_commands[1:]) + ')'
+                    if listen_commands[0] in dir(self)[3:]:
+                        try:
+                            exec('result = self.' + listen_commands[0] + listen_args)
+                            if result:
+                                conn.sendall(str(result) + '\n')
+                            else:
+                                conn.sendall('NO DATA\n')
+                        except TypeError:
+                            conn.sendall('ERROR: TYPE ERROR\n')
+                    else:
+                        conn.sendall('ERROR: COMMAND ERROR\n')
+                except Exception:
+                    if len(self.error_list) < 21:
+                        err = traceback.format_exc()
+                        print 'ERROR in START thread:'
+                        print err
+                        self.error_list.append(err)
+                    time.sleep(0.5)
 
     def start(self):
+        self.error_list = []
+        self.read_flag = 1
         while self.on_flag:
-            self.inst.write('smu.measure.read()')
+            try:
+                if self.read_flag:
+                    self.inst.write('smu.measure.read()')
+            except Exception:
+                if len(self.error_list) < 21:
+                    err = traceback.format_exc()
+                    print 'ERROR in START thread:'
+                    print err
+                    self.error_list.append(err)
             time.sleep(0.5)
 
     def stop(self):
@@ -74,29 +93,36 @@ class keithley2450:
     def close(self):
         self.stop()
 
+    def write_to_instrument(self, *messages):
+        self.read_flag = 0
+        for message in messages:
+            self.inst.write(message)
+        time.sleep(0.01)
+        self.read_flag = 1
+
     def output_on(self):
-        self.inst.write('smu.source.output = smu.ON')
+        self.write_to_instrument('smu.source.output = smu.ON')
 
     def output_off(self):
-        self.inst.write('smu.source.output = smu.OFF')
+        self.write_to_instrument('smu.source.output = smu.OFF')
 
     def set_voltage(self, num):
-        self.inst.write('smu.source.level = ' + str(num))
+        self.write_to_instrument('smu.source.level = ' + str(num))
 
     def set_source_range(self, num):
-        self.inst.write('smu.source.range = ' + str(num))
+        self.write_to_instrument('smu.source.range = ' + str(num))
 
     def set_source_autorange(self):
-        self.inst.write('smu.source.autorange = smu.ON')
+        self.write_to_instrument('smu.source.autorange = smu.ON')
 
     def set_measure_range(self,num):
-        self.inst.write('smu.measure.range = ' + str(num))
+        self.write_to_instrument('smu.measure.range = ' + str(num))
 
     def set_measure_autorange(self):
-        self.inst.write('smu.measure.autorange = smu.ON')
+        self.write_to_instrument('smu.measure.autorange = smu.ON')
 
     def set_current_limit(self,num):
-        self.inst.write('smu.source.ilimit.level = ' + str(num))
+        self.write_to_instrument('smu.source.ilimit.level = ' + str(num))
 
     def set_overprotection(self,num):
         protection_levels = [2, 5, 10, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
@@ -104,52 +130,39 @@ class keithley2450:
             protection_value = num
         else:
             if num <= 2:
-                self.inst.write('smu.source.protect.level = smu.PROTECT_2V')
+                self.write_to_instrument('smu.source.protect.level = smu.PROTECT_2V')
                 protection_value = 2
             else:
                 protect_levels_new = protection_levels[:]
                 protect_levels_new.append(num)
                 protection_value = protection_levels[sorted(protect_levels_new).index(num)-1]
         if protection_value == 200:
-            self.inst.write('smu.source.protect.level = smu.PROTECT_NONE')
+            self.write_to_instrument('smu.source.protect.level = smu.PROTECT_NONE')
             print 'Set Overprotection to NONE'
         else:
-            self.inst.write('smu.source.protect.level = smu.PROTECT_' + str(protection_value) + 'V')
+            self.write_to_instrument('smu.source.protect.level = smu.PROTECT_' + str(protection_value) + 'V')
             print 'Set Overprotection to ' + str(protection_value) + 'V'
 
-    def read_voltage(self):
+    def read_from_instrument(self, *messages):
+        self.read_flag = 0
         while True:
             try:
                 self.inst.write('*CLS')
-                self.inst.write('smu.measure.read(defbuffer1)')
-                self.inst.write('print(defbuffer1.sourcevalues[defbuffer1.endindex])')
+                for message in messages:
+                    self.inst.write(message)
                 result = float(self.inst.query('').strip())
                 self.inst.write('*CLS')
                 break
             except ValueError:
                 pass
+        self.read_flag = 1
         return result
+
+    def read_voltage(self):
+        return self.read_from_instrument('smu.measure.read(defbuffer1)','print(defbuffer1.sourcevalues[defbuffer1.endindex])')
 
     def read_current(self):
-        while True:
-            try:
-                self.inst.write('*CLS')
-                self.inst.write('print(smu.measure.read(defbuffer1))')
-                result = float(self.inst.query('').strip())
-                self.inst.write('*CLS')
-                break
-            except ValueError:
-                pass
-        return result
+        return self.read_from_instrument('print(smu.measure.read(defbuffer1))')
 
     def read_setpoint(self):
-        while True:
-            try:
-                self.inst.write('*CLS')
-                self.inst.write('print(smu.source.level)')
-                result = float(self.inst.query('').strip())
-                self.inst.write('*CLS')
-                break
-            except ValueError:
-                pass
-        return result
+        return self.read_from_instrument('print(smu.source.level)')
