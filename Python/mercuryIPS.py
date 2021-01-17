3d7cff2fff25c0352ff9c446732b9409e9bffe95
dilwong <dilwong@gmail.com>
2021-01-17 01:57:10 -0500

    Modified mercuryIPS.py to enable control of magnet via LabView; Added magnet_controller.vi

--- Python/mercuryIPS.py
+++ Python/mercuryIPS.py
@@ -2,6 +2,8 @@
 
 import serial
 import time
+import socket
+import traceback
 try:
     import thread
 except:
@@ -15,10 +17,91 @@ class mercuryIPS:
         self.x = vectorDirection(self, 'X')
         self.y = vectorDirection(self, 'Y')
         self.z = vectorDirection(self, 'Z')
+        self.on_flag = 1
+        self.__listen_state__ = 0
+        self.error_list = []
 
     @atexit.register
     def exit_handler():
         self.close()
+
+    def listen(self):
+        def parse_value(value):
+            try:
+                return float(value)
+            except:
+                return value
+        self.__listen_state__ = 1
+        host = '127.0.0.1'
+        port = 65242
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.lis_sock = s
+        s.bind((host, port))
+        s.listen(0)
+        while self.on_flag:
+            conn, addr = s.accept()
+            listen_string = conn.recv(1024)
+            if listen_string == 'QUIT':
+                s.close()
+            else:
+                if self.on_flag == 0:
+                    break
+                try:
+                    if listen_string[-1] == '\n':
+                        listen_commands = listen_string.strip().split()
+                        if listen_commands[0].upper() == 'X':
+                            dir_obj = self.x
+                        elif listen_commands[0].upper() == 'Y':
+                            dir_obj = self.y
+                        elif listen_commands[0].upper() == 'Z':
+                            dir_obj = self.z
+                        else:
+                            print("LISTEN COMMAND MALFORMED. IGNORING COMMAND.")
+                            conn.sendall('ERROR: COMMAND ERROR. DIRECTION NOT X, Y, OR Z\n')
+                        try:
+                            comm_to_exec = dir_obj.__commands__[listen_commands[1].lower()]
+                            if comm_to_exec.__name__ != listen_commands[1].lower():
+                                raise KeyError
+                            if comm_to_exec.im_self.__direction__ != listen_commands[0].upper():
+                                raise KeyError
+                        except KeyError:
+                            print("LISTEN COMMAND MALFORMED. IGNORING COMMAND.")
+                            conn.sendall('ERROR: COMMAND ERROR. COMMAND NOT RECOGNIZED\n')
+                        try:
+                            args_list = [parse_value(val) for val in listen_commands[2:]]
+                            result = None
+                            result = comm_to_exec(*args_list)
+                            print("EXECUTED:")
+                            print("\tDIRECTION: " + listen_commands[0].upper())
+                            print("\tCOMMAND: " + listen_commands[1].lower())
+                            print("\tARGUMENTS: " + ' '.join(listen_commands[2:]))
+                            if result is None:
+                                conn.sendall('NO DATA\n')
+                            else:
+                                conn.sendall(str(result) + '\n')
+                        except TypeError:
+                            print("LISTEN COMMAND MALFORMED. IGNORING COMMAND.")
+                            conn.sendall('ERROR: COMMAND ERROR. WRONG NUMBER OF ARGUMENTS\n')
+                        except magnetException as e:
+                            print("LISTEN COMMAND ERROR. MAGNET EXCEPTION DETECTED.")
+                            conn.sendall('ERROR: MAGNET EXCEPTION: ' + str(e) + '\n')
+                    else:
+                        print("LISTEN COMMAND MALFORMED. IGNORING COMMAND.")
+                        conn.sendall('ERROR: COMMAND ERROR. NEEDS LINE FEED\n')
+                    dir_obj = None
+                except Exception:
+                    if len(self.error_list) < 21:
+                        err = traceback.format_exc()
+                        print(err)
+                        self.error_list.append(err)
+                    time.sleep(0.5)
+        self.__listen_state__ = 0
+
+    def start_listen(self):
+        if self.__listen_state__ == 0:
+            thread.start_new_thread(self.listen,())
+        else:
+            print("MAGNET LISTEN LOOP ALREADY RUNNING.")
 
     def query(self, command):
         self.__power_supply__.write(command)
@@ -29,6 +112,9 @@ class mercuryIPS:
             raise magnetException('Direction is not X, Y, or Z')
         reply = self.query('SET:DEV:GRP' + direction.upper() + ':PSU:' + command_abbrev + '\r\n')
         result = reply.strip().split(':')[-1]
+        if result == '':
+            # print("WARNING: EMPTY STRING RETURNED. CHECK CONNECTION TO MAGNET CONTROLLER")
+            raise magnetException('Empty string returned. Check connection to magnet controller')
         if result == 'INVALID':
             print('ERROR: INVALID QUERY')
         return result
@@ -38,6 +124,8 @@ class mercuryIPS:
             raise magnetException('Direction is not X, Y, or Z')
         reply = self.query('READ:DEV:GRP' + direction.upper() + ':PSU:' + command_abbrev + '\r\n')
         result = reply.strip().split(':')[-1]
+        if result == '':
+            raise magnetException('Empty string returned. Check connection to magnet controller')
         if result == 'INVALID':
             print('ERROR: INVALID QUERY')
         return result
@@ -53,7 +141,12 @@ class mercuryIPS:
             raise magnetException('Incorrect unit')
 
     def close(self):
+        self.on_flag = 0
         self.__power_supply__.close()
+        try:
+            self.lis_sock.close()
+        except:
+            pass
 
     def read_state(self, direction):
         return self.read(direction, 'ACTN?')
@@ -62,14 +155,10 @@ class mercuryIPS:
         return self.set(direction, 'ACTN:HOLD')
 
     def ramp_to_set(self, direction):
-        print('ERROR: NOT IMPLEMENTED YET')
-        # return = self.set(direction, 'ACTN:RTOS')
-        return 'NULL'
+        return self.set(direction, 'ACTN:RTOS')
 
     def ramp_to_zero(self, direction):
-        print('ERROR: NOT IMPLEMENTED YET')
-        # return = self.set(direction, 'ACTN:RTOZ')
-        return 'NULL'
+        return self.set(direction, 'ACTN:RTOZ')
 
     def read_switch_heater(self, direction):
         return self.read(direction, 'SIG:SWHT?')
@@ -115,6 +204,21 @@ class vectorDirection:
     def __init__(self, IPS_instance, direction):
         self.__IPS_instance__ = IPS_instance
         self.__direction__ = direction
+        self.__commands__ = {"read_state": self.read_state, \
+                            "hold": self.hold, \
+                            "ramp_to_set": self.ramp_to_set, \
+                            "ramp_to_zero": self.ramp_to_zero, \
+                            "read_switch_heater": self.read_switch_heater, \
+                            "switch_heater_on": self.switch_heater_on, \
+                            "switch_heater_off": self.switch_heater_off, \
+                            "read_voltage": self.read_voltage, \
+                            "read_current": self.read_current, \
+                            "read_field": self.read_field, \
+                            "read_persistent_field": self.read_persistent_field, \
+                            "read_target_field": self.read_target_field, \
+                            "set_target_field": self.set_target_field, \
+                            "read_ramp_rate": self.read_ramp_rate, \
+                            "set_ramp_rate": self.set_ramp_rate}
 
     def read_state(self):
         return self.__IPS_instance__.read_state(self.__direction__)
@@ -153,6 +257,8 @@ class vectorDirection:
         return self.__IPS_instance__.read_target_field(self.__direction__)
 
     def set_target_field(self, value):
+        if (type(value) != float) and (type(value) != int):
+            raise magnetException('Input value is not a number')
         if (self.__direction__ == 'X') or (self.__direction__ == 'X'):
             if abs(value) > 1:
                 raise magnetException('Exceeds field limit')
@@ -165,6 +271,8 @@ class vectorDirection:
         return self.__IPS_instance__.read_ramp_rate(self.__direction__)
 
     def set_ramp_rate(self, value):
+        if (type(value) != float) and (type(value) != int):
+            raise magnetException('Input value is not a number')
         if value < 0:
             raise magnetException('Do not set a negative ramp rate')
         if value > 0.5:
